8bf1530528d90607d4fa975c3d4a8c1ecadf1c4b
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-05-15 10:22:16 -0700

    Attempting to commit to new repository

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import SearchBar from "./SearchBar/SearchBar";
+import FilterableList from "./FilterableList/FilterableList";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <FilterableList files={this.props.files} />
+      </div>
+    );
+  }
+}
+
+export default App;
